7c85beedd7420f23374f915dbecb3ccc9714f317
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-09-11 13:17:11 +0200

    yes

--- loops/app.js
+++ loops/app.js
@@ -2,19 +2,26 @@ let index = 0
 
 let names = ["Niklas", "Oliver", "Emil", "Melker", "Alex"]
 
-// for (let i = names.length - 1; i >= 0; i--) {
-//     console.log(names[i])
-// }
+for (let i = names.length - 1; i >= 0; i--) {
+    // console.log(names[i])
+}
 
 let foundNiklas = false
 
-while (!foundNiklas) {
-    if (names[index] === "Niklas") {
-        foundNiklas = true
-    }
-    else {
-        index++
-    }
-}
+// while (!foundNiklas) {
+//     if (names[index] === "Niklas") {
+//         foundNiklas = true
+//     }
+//     else {
+//         index++
+//     }
+// }
 
-console.log("I found Niklas at index " + index)
+// console.log("I found Niklas at index " + index)
+
+for (let i = 0; i <= 100; i += 2) {
+    console.log(i)
+}
+for (let a = 5; a <= 79; a /= 3) {
+    console.log(a)
+}
