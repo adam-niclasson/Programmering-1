f254a896bdde6f02836cc7384c4913112ef2e222
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-09-15 09:09:03 +0200

    Buzz
    DOne

---
+++ loops/FizzBuzz.js
@@ -0,0 +1,18 @@
+// for (let i = 0; i <= 100; i += 1) {
+//     console.log(i)
+// }
+
+for (let a = 0; a <= 100; a++) {
+    if (a % 2 == 0) {
+        console.log(a)
+    }
+    else if (a % 3 == 0 && a % 5 == 0) {
+        console.log("FizzBuzz")
+    }
+    else if (a % 3 == 0) {
+        console.log("Fizz")
+    }
+    else if (a % 5 == 0) {
+        console.log("Buzz")
+    }
+}
