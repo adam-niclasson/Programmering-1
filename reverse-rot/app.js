3dbf3809b39e622d6d029ddd2489e8483ff8127c
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-09-29 09:27:11 +0200

    yes

---
+++ reverse-rot/app.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var alf = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,R,S,T,U,V,W,X,Y,Z,_,."]
+
+var a = readline.question('Enter Text Here?: ');
+var ans;
+
