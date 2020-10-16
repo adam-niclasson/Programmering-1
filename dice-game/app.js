9c5a65494ceafd8ee85f17ab08d62db4fb802a5f
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-10-16 13:32:48 +0200

    yes

--- dice-game/app.js
+++ dice-game/app.js
@@ -12,9 +12,13 @@ function yeet() {
     let s2 = Number(lines[1])
     let s3 = Number(lines[2])
     let s4 = Number(lines[3])
+    let s5 = Number(lines[4])
+    let s6 = Number(lines[5])
+    let s7 = Number(lines[6])
+    let s8 = Number(lines[7])
 
-    let d1 = (s1 + s2) / 2
-    let d2 = (s3 + s4) / 2
+    let d1 = ((s1 + s2) / 2) + ((s3 + s4) / 2)
+    let d2 = ((s5 + s6) / 2) + ((s7 + s8) / 2)
 
     if (d1 > d2) {
         console.log("Emma scored " + d1 + " and won!");
@@ -27,7 +31,7 @@ function yeet() {
 
 rl.on('line', (input) => {
     lines.push(input)
-    if (lines.length == 4) {
+    if (lines.length == 8) {
         yeet(); {
             rl.close()
         }
---
+++ gissa-talet/app.js
@@ -0,0 +1,4 @@
+let answer = Math.floor((Math.random() * 10) + 1)
+let tries = 5;
+let question = "Guess a number between 1 and 10";
+let guess = '';
