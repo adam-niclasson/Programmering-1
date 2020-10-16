e852f290bedb2024dc759ff4c8fa1e7c9ae9f75a
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-10-16 13:33:41 +0200

    yes

--- gissa-talet/app.js
+++ gissa-talet/app.js
@@ -2,3 +2,18 @@ let answer = Math.floor((Math.random() * 10) + 1)
 let tries = 5;
 let question = "Guess a number between 1 and 10";
 let guess = '';
+
+while (tries > 0) {
+    guess = prompt(question, guess);
+
+    if (guess == null) {
+        alert('You lost. Better luck next time!');
+        break;
+    }
+    else if (isFinite(guess) && guess != '') {
+        guess = +guess;
+    }
+    else if (a) {
+        a
+    }
+}
