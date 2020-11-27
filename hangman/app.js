b3aa940eeb8704fb34db4551713dd109548d5d1c
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-11-27 12:47:51 +0100

    yes

--- hangman/app.js
+++ hangman/app.js
@@ -1,7 +1,7 @@
 // 1. Ge spelaren instruktioner om hur man spelar spelet.
 // 2. Generera ett slumpat ord som spelaren ska gissa på.
 //    2.1 Använd ett eget ord som du själv lägger in.
-// 3. Skapa X antal "_" som matcher antalet bokstäver i ordet och be spelaren att gissa EN boksgtaav i taget.
+// 3. Skapa X antal "\_" som matcher antalet bokstäver i ordet och be spelaren att gissa EN boksgtaav i taget.
 //    3.1 Meddela att spelaren gissat rätt eller fel.
 // 4. Spara spelarens gissnignar.
 // 5. Grattis du är klar.
@@ -15,12 +15,14 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let guess = Math.round(Math.random() * 10 + 1);
+const words = ["sverige, musik, bilder, båtar, cyckel, skola"]
+
+let guess = words(Math.random());
 let tries = 7;
 
 rl.on('line', (input) => {
     let ans = parseInt(input);
-    
+
     tries--;
     if (tries == 0) {
         console.log('You lost and the game has ended.')
