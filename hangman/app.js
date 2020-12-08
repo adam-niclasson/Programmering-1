d999b40901b9c9fc132a53a910241eb550346c0b
adam-niclasson <adam-niclasson@users.noreply.github.com>
2020-12-08 08:33:38 +0100

    yes

--- hangman/app.js
+++ hangman/app.js
@@ -1,12 +1,17 @@
-// 1. Ge spelaren instruktioner om hur man spelar spelet.
-// 2. Generera ett slumpat ord som spelaren ska gissa på.
-//    2.1 Använd ett eget ord som du själv lägger in.
-// 3. Skapa X antal "_" som matcher antalet bokstäver i ordet och be spelaren att gissa EN boksgtaav i taget.
-//    3.1 Meddela att spelaren gissat rätt eller fel.
-// 4. Spara spelarens gissnignar.
-// 5. Grattis du är klar.
+// 1. ge instruktioner till spelaren om hänga gubbe
+// 2. skapa en array med ord som slumpas med hjälp av math.random
+//    2.1 använd ett ord som du själv lägger in(valfritt)
+// 3. skapa x antal _ som ersätter bokstäverna
+//    3.1 om spelaren gissar rätt bokstav så ersätts _ med ex "a"
+//    3.2 gissar spelaren fel så stannar _ som _
+// 4. meddela spelaren om gissningen är rätt respektive fel
+// 5. spelaren får bara gissa EN bokstav i taget
+// 6. spara spelarens gissning så både programmet kommer ihåg vad spelaren gissat och även så spelaren kan se sina gissningar.
+// 7. är hela ordet gissat innan dem 7 gissningarna är slut så avslutas spelet och spelaren vinner
+//    7.1 om spelaren får slut på gissningar så avslutas spelet och spelaren har förlorat.
+// 8. du är nu klar, börja programmera och problemsök
+// 9. Let the game begin :)
 
-//Let the guessing game begin
 
 const readline = require('readline');
 
@@ -25,6 +30,8 @@ function rules() {
 }
 //array of listed words the player can guess
 const words = ['sverige', 'musik', 'bilder', 'båtar', 'cyckel', 'skola']
+//the placeholder of the randomized word.
+let selected = []
 //splits the randomized word so the player is able to guess one letter at a time
 function guessingWord() {
     let randomWords = words[Math.floor(words.length * Math.random())]
